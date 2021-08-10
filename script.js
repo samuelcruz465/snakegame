8576245ff3a44edb796a9a1502481a2930651a27
samuelcruz465 <samuelcruz465@gmail.com>
2021-08-09 22:38:33 -0300

    tamanho da cobrinha

--- script.js
+++ script.js
@@ -58,8 +58,15 @@ function iniciarjogo(){
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
+
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
     
-    snake.pop();
+    
 
     let newHead = {
         x: snakeX,
