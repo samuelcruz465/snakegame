ea36c7f5265f75efed55e70ce2fdbc8b52bedcbc
samuelcruz465 <samuelcruz465@gmail.com>
2021-08-09 22:13:36 -0300

    movimento

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightgreen"
@@ -21,5 +22,33 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarjogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+    
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead){
+        
+    }
+
+
+    
+
+}
+
+let jogo = setInterval(iniciarjogo, 100);
+
