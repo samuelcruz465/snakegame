d4c88e9333889500bcd1f5a145798d7eb42703b9
samuelcruz465 <samuelcruz465@gmail.com>
2021-08-09 21:11:00 -0300

    cobrina criada

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let canvas = document.getElementById("snake");
 //renderiza desenho context
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 
 function criarBG(){
@@ -9,4 +14,12 @@ function criarBG(){
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobrinha(){
+    for(i=0; i< snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
